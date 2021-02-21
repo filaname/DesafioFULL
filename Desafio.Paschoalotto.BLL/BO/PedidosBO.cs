68108582128c794ed08ab8efe89f71dd1c12f54f
Rodolfo R Orlandini <rodolfo.orlandini@hotmail.com>
2021-02-20 22:23:49 -0300

    Upload do projeto.

---
+++ Desafio.Paschoalotto.BLL/BO/PedidosBO.cs
@@ -0,0 +1,27 @@
+﻿using Desafio.Paschoalotto.DAL.DAO;
+using Desafio.Paschoalotto.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Desafio.Paschoalotto.BLL.BO
+{
+    public class TitulosBO
+    {
+        TitulosDAO titulosDAO;
+
+        public TitulosBO()
+        {
+            titulosDAO = new TitulosDAO();
+        }
+
+        public async Task<bool> SalvarTitulo(Titulos titulo)
+        {
+            return await titulosDAO.SavarTituloAsync(titulo);
+        }
+
+        public async Task<List<Titulos>> BuscarTitulo(string Nome)
+        {
+            return await titulosDAO.ListarTituloAsync(Nome);
+        }
+    }
+}
---
+++ Desafio.Paschoalotto.DAL/Tabelas.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.Paschoalotto.DAL.Entidades;
+
+namespace Desafio.Paschoalotto.DAL
+{
+    public class Tabelas : Database
+    {
+        public Tabelas()
+        {
+            _database.CreateTableAsync<Titulos>().Wait();
+            _database.CreateTableAsync<Parcelas>().Wait();
+        }
+    }
+}
